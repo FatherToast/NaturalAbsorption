8a38cb68099d5262459a5bc049f61aa1f7529557
Sarinsa <mikkemusbruker@gmail.com>
2022-01-08 19:16:03 +0100

    Created static fields for absorption data NBT tag keys to API

--- src/main/java/fathertoast/naturalabsorption/api/INaturalAbsorption.java
+++ src/main/java/fathertoast/naturalabsorption/api/INaturalAbsorption.java
@@ -3,9 +3,25 @@ package fathertoast.naturalabsorption.api;
 import net.minecraft.entity.player.PlayerEntity;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public interface INaturalAbsorption {
+
+    /**
+     * These are all the tag keys the mod uses for
+     * storing the player's absorption data.
+     *
+     * This data can easily be read and
+     * manipulated through the IHeartData
+     * interface, but can also be tinkered
+     * with manually by getting the player's
+     * persistent data tag and using these keys.
+     */
+    String TAG_BASE = "naturalabsorption";
+    String TAG_NATURAL_ABSORPTION = "AbsorbNatural";
+    String TAG_EQUIPMENT_ABSORPTION = "AbsorbEquip";
+    String TAG_DELAY_ABSORPTION = "AbsorbDelay";
+    String TAG_DELAY_HEALTH = "HealthDelay";
+
     /**
      * Must be called server-side. Throws an
      * IllegalArgumentException if called on client.
