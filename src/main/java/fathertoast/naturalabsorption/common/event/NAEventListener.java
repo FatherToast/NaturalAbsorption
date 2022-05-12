ffbdfb2dff4e015b359f402ddc187e0568396297
Sarinsa <mikkemusbruker@gmail.com>
2022-05-12 15:31:45 +0200

    Fixed Absorption Book recipe not being awarded when supposed to

--- src/main/java/fathertoast/naturalabsorption/common/event/NAEventListener.java
+++ src/main/java/fathertoast/naturalabsorption/common/event/NAEventListener.java
@@ -16,23 +16,23 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class NAEventListener {
 
-    private static final ResourceLocation ADV_BOOK_RECIPE = NaturalAbsorption.resourceLoc(
-            "recipes/" + NAItems.ABSORPTION_BOOK.getId().getPath() );
-    
+    private static final ResourceLocation ADV_BOOK_RECIPE = NaturalAbsorption.resourceLoc("recipes/" + NAItems.ABSORPTION_BOOK.getId().getPath());
+
+
     @SubscribeEvent( priority = EventPriority.NORMAL )
-    public void onAdvancement( AdvancementEvent event ) {
-        if( HeartManager.isAbsorptionEnabled() && Config.ABSORPTION.NATURAL.upgradeBookRecipe.get() != BookRecipeCondition.Type.NONE ) {
-            if( event.getAdvancement().getId().equals( ADV_BOOK_RECIPE ) ) {
+    public void onAdvancement(AdvancementEvent event) {
+        if(HeartManager.isAbsorptionEnabled() && Config.ABSORPTION.NATURAL.upgradeBookRecipe.get() != BookRecipeCondition.Type.NONE) {
+            if( event.getAdvancement().getId().equals( ADV_BOOK_RECIPE )) {
                 // The advancement for unlocking the book of absorption recipe
                 ResourceLocation recipe = new ResourceLocation(
-                        NaturalAbsorption.toString( NAItems.ABSORPTION_BOOK.get().getRegistryName() ) + "_" +
+                        NaturalAbsorption.toString(NAItems.ABSORPTION_BOOK.get().getRegistryName()) + "_" +
                                 Config.ABSORPTION.NATURAL.upgradeBookRecipe.get().name().toLowerCase()
                 );
                 try {
-                    event.getPlayer().awardRecipesByKey( new ResourceLocation[] { recipe } );
+                    event.getPlayer().awardRecipesByKey(new ResourceLocation[] {recipe});
                 }
-                catch( Exception e ) {
-                    NaturalAbsorption.LOG.warn( "Something went wrong trying to award a player the absorption book recipe! Aw man :(" );
+                catch(Exception e) {
+                    NaturalAbsorption.LOG.warn("Something went wrong trying to award a player the absorption book recipe! Aw man :(");
                     e.printStackTrace();
                 }
             }
