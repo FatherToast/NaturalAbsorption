094445fb05ca64b341ab7232450e5589c59e7484
FatherToast <father.toast@hotmail.com>
2022-06-13 22:22:38 -0500

    Last config look-through

--- src/main/java/fathertoast/naturalabsorption/common/enchantment/AbsorptionEnchantment.java
+++ src/main/java/fathertoast/naturalabsorption/common/enchantment/AbsorptionEnchantment.java
@@ -17,42 +17,42 @@ class AbsorptionEnchantment extends Enchantment {
     };
     
     public AbsorptionEnchantment() {
-        super(Config.EQUIPMENT.ENCHANTMENT.rarity.get().parentValue, Config.EQUIPMENT.ENCHANTMENT.slot.get().parentValue, VALID_SLOTS);
+        super( Config.EQUIPMENT.ENCHANTMENT.rarity.get().parentValue, Config.EQUIPMENT.ENCHANTMENT.slot.get().parentValue, VALID_SLOTS );
     }
-
+    
     /** @return The maximum absorption granted by enchantments. */
-    public static float getMaxAbsorptionBonus(PlayerEntity player) {
+    public static float getMaxAbsorptionBonus( PlayerEntity player ) {
         // Calculate enchantment level
         int enchantLevel;
-
-        if(Config.EQUIPMENT.ENCHANTMENT.stacking.get()) {
+        
+        if( Config.EQUIPMENT.ENCHANTMENT.stacking.get() ) {
             Iterable<ItemStack> equipment = player.getArmorSlots();
             enchantLevel = 0;
-
-            for(ItemStack itemStack : equipment) {
-                enchantLevel += EnchantmentHelper.getItemEnchantmentLevel(NAEnchantments.ABSORPTION_ENCHANTMENT.get(), itemStack);
+            
+            for( ItemStack itemStack : equipment ) {
+                enchantLevel += EnchantmentHelper.getItemEnchantmentLevel( NAEnchantments.ABSORPTION_ENCHANTMENT.get(), itemStack );
             }
         }
         else {
-            enchantLevel = EnchantmentHelper.getEnchantmentLevel(NAEnchantments.ABSORPTION_ENCHANTMENT.get(), player);
+            enchantLevel = EnchantmentHelper.getEnchantmentLevel( NAEnchantments.ABSORPTION_ENCHANTMENT.get(), player );
         }
-
+        
         // Calculate capacity to grant for level
-        if(enchantLevel > 0) {
-            return Math.max(0.0F, (float) Math.min(
+        if( enchantLevel > 0 ) {
+            return Math.max( 0.0F, (float) Math.min(
                     Config.EQUIPMENT.ENCHANTMENT.potencyBase.get() + Config.EQUIPMENT.ENCHANTMENT.potencyPerLevel.get() * enchantLevel,
-                    Config.EQUIPMENT.ENCHANTMENT.potencyMax.get()));
+                    Config.EQUIPMENT.ENCHANTMENT.potencyMax.get() ) );
         }
         return 0.0F;
     }
     
     @Override
-    public int getMinCost(int level) {
+    public int getMinCost( int level ) {
         return Config.EQUIPMENT.ENCHANTMENT.costBase.get() + (level - 1) * Config.EQUIPMENT.ENCHANTMENT.costPerLevel.get();
     }
     
     @Override
-    public int getMaxCost(int level) { return getMinCost(level + 1); }
+    public int getMaxCost( int level ) { return getMinCost( level + 1 ); }
     
     @Override
     public int getMaxLevel() { return Config.EQUIPMENT.ENCHANTMENT.levelMax.get(); }
@@ -62,4 +62,7 @@ class AbsorptionEnchantment extends Enchantment {
     
     @Override
     public boolean isTreasureOnly() { return Config.EQUIPMENT.ENCHANTMENT.treasureOnly.get(); }
+    
+    @Override
+    public boolean isDiscoverable() { return Config.EQUIPMENT.ENCHANTMENT.enabled.get(); }
 }
