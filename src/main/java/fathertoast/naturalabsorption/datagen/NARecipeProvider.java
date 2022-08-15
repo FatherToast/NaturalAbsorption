7be15abc69fb42228e76e044e7d47e83303f68c3
Sarinsa <mikkemusbruker@gmail.com>
2022-08-15 18:37:24 +0200

    Fixed conditional recipe

--- src/main/java/fathertoast/naturalabsorption/datagen/NARecipeProvider.java
+++ src/main/java/fathertoast/naturalabsorption/datagen/NARecipeProvider.java
@@ -7,7 +7,12 @@ import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
 import net.minecraft.data.ShapelessRecipeBuilder;
 import net.minecraft.item.Items;
+import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.crafting.ConditionalAdvancement;
+import net.minecraftforge.common.crafting.ConditionalRecipe;
+import net.minecraftforge.common.crafting.conditions.ICondition;
+import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import slimeknights.tconstruct.common.TinkerTags;
@@ -34,13 +39,7 @@ public class NARecipeProvider extends RecipeProvider {
                 .unlockedBy( "has_sponge", has( Items.SPONGE ) )
                 .save( consumer );
         
-        this.handleDependencyRecipes( consumer );
-    }
-    
-    private void handleDependencyRecipes( Consumer<IFinishedRecipe> consumer ) {
-        if( ModList.get().isLoaded( "tconstruct" ) ) {
-            this.tinkersRecipes( consumer );
-        }
+        this.tinkersRecipes( consumer );
     }
     
     private void tinkersRecipes( Consumer<IFinishedRecipe> consumer ) {
@@ -54,14 +53,42 @@ public class NARecipeProvider extends RecipeProvider {
         final String defenseSalvage = "tools/modifiers/salvage/defense/";
         final String compatFolder = "tools/modifiers/compat/";
         final String compatSalvage = "tools/modifiers/salvage/compat/";
-        
-        ModifierRecipeBuilder.modifier( NAModifiers.ARMOR_ABSORPTION.get() )
-                .addInput( NAItems.ABSORPTION_BOOK.get() )
-                .addSalvage( NAItems.ABSORPTION_BOOK.get(), 0.5f )
-                .setTools( TinkerTags.Items.ARMOR )
-                .setSlots( SlotType.DEFENSE, 1 )
-                .setMaxLevel( 3 )
-                .build( consumer, prefix( "tconstruct", NAModifiers.ARMOR_ABSORPTION, defenseFolder ) );
+
+        modDependentRecipe( recipeConsumer -> {
+            ModifierRecipeBuilder.modifier( NAModifiers.ARMOR_ABSORPTION.get() )
+                    .addInput( NAItems.ABSORPTION_BOOK.get() )
+                    .addSalvage( NAItems.ABSORPTION_BOOK.get(), 0.5f )
+                    .setTools( TinkerTags.Items.ARMOR )
+                    .setSlots( SlotType.DEFENSE, 1 )
+                    .setMaxLevel( 3 )
+                    .build( recipeConsumer, prefix( "tconstruct", NAModifiers.ARMOR_ABSORPTION, defenseFolder ) );
+        }, consumer, false, "tconstruct");
+    }
+
+
+    public void modDependentRecipe( Consumer<Consumer<IFinishedRecipe>> recipeBuilder, Consumer<IFinishedRecipe> consumer, boolean advancement, String... modIds ) {
+        final ConditionalRecipe.Builder builder = ConditionalRecipe.builder( );
+        final ConditionalAdvancement.Builder advBuilder = ConditionalAdvancement.builder( );
+
+        for ( String modid : modIds ) {
+            ICondition condition = new ModLoadedCondition( modid );
+            builder.addCondition( condition );
+            advBuilder.addCondition( condition );
+        }
+
+        if ( advancement ) {
+            recipeBuilder.accept(finishedRecipe -> {
+                builder.addRecipe(finishedRecipe)
+                        .setAdvancement(advBuilder.addAdvancement(finishedRecipe))
+                        .build(consumer, finishedRecipe.getId());
+            });
+        }
+        else {
+            recipeBuilder.accept(finishedRecipe -> {
+                builder.addRecipe(finishedRecipe)
+                        .build(consumer, finishedRecipe.getId());
+            });
+        }
     }
     
     
