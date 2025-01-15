fea8315f2ec449b7bf2816f676f16cc110b91851
Sarinsa <mikkemusbruker@gmail.com>
2025-01-16 00:01:21 +0100

    Bumped Crust version and Forge version and yeah

--- src/main/java/fathertoast/naturalabsorption/common/config/Config.java
+++ src/main/java/fathertoast/naturalabsorption/common/config/Config.java
@@ -1,21 +1,21 @@
 package fathertoast.naturalabsorption.common.config;
 
 import fathertoast.crust.api.config.common.ConfigManager;
+import fathertoast.naturalabsorption.common.core.NaturalAbsorption;
 
 /**
  * The initial loading for this is done during the common setup event.
  */
 public class Config {
 
-    private static final ConfigManager MANAGER = ConfigManager.create( "NaturalAbsorption" );
+    private static final ConfigManager MANAGER = ConfigManager.create( "NaturalAbsorption", NaturalAbsorption.MOD_ID );
     
     public static final MainConfig MAIN = new MainConfig( MANAGER, "main" );
     public static final AbsorptionConfig ABSORPTION = new AbsorptionConfig( MANAGER, "absorption" );
     public static final EquipmentConfig EQUIPMENT = new EquipmentConfig( MANAGER, "enchant_and_armor" );
     public static final HealthConfig HEALTH = new HealthConfig( MANAGER, "health" );
     public static final CompatConfig COMPAT = new CompatConfig( MANAGER, "compat");
-    //public static final HungerConfig HUNGER = new MainConfig( CONFIG_DIR, "hunger" );
-    
+
     /** Performs initial loading of all configs in this mod. */
     public static void initialize() {
         MAIN.SPEC.initialize();
@@ -23,6 +23,5 @@ public class Config {
         EQUIPMENT.SPEC.initialize();
         HEALTH.SPEC.initialize();
         COMPAT.SPEC.initialize();
-        //HUNGER.SPEC.initialize();
     }
 }
