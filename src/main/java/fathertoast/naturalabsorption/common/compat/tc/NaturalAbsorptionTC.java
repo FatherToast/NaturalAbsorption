012c870f704deb2aca28f7774c9f44d011616cc3
Sarinsa <mikkemusbruker@gmail.com>
2026-08-29 19:35:25 +0200

    Javadoc bink

--- src/main/java/fathertoast/naturalabsorption/common/compat/tc/NaturalAbsorptionTC.java
+++ src/main/java/fathertoast/naturalabsorption/common/compat/tc/NaturalAbsorptionTC.java
@@ -1,10 +1,26 @@
 package fathertoast.naturalabsorption.common.compat.tc;
 
+import fathertoast.naturalabsorption.common.core.NaturalAbsorption;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-public class NaturalAbsorptionTC {
+public final class NaturalAbsorptionTC {
     
+    /** Tinkers Construct mod ID. */
+    public static final String MOD_ID = "tconstruct";
+    
+    
+    /**
+     * Called from Natural Absorption's {@link NaturalAbsorption#NaturalAbsorption(FMLJavaModLoadingContext) mod class constructor}
+     * to initialize Tinkers Construct compat.
+     */
     public static void init( IEventBus modBus ) {
-        modBus.addListener( NAModifiers::onModifierRegister );
+        if( ModList.get().isLoaded( MOD_ID ) ) {
+            modBus.addListener( NAModifiers::onModifierRegister );
+        }
     }
+    
+    // Non-instantiable
+    private NaturalAbsorptionTC() { }
 }
